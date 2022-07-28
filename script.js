f7b261214d68f48c570eecb9bc1d2c9dadf63729
Claire Roberts <clairefinnie@gmail.com>
2022-07-27 22:26:55 -0400

    added game function to loop singleRound five times, and refactored code to get inputs within the round

--- script.js
+++ script.js
@@ -6,20 +6,49 @@ function getComputerChoice () {
     return computerChoice;
 }
 
-const computerSelection = getComputerChoice();
-let playerSelection = prompt("Choose your move").toLowerCase();
 
-while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
-    playerSelection = prompt("Please enter rock, paper or scissors");}
+let winner = "";
 
-function singleRound (computerSelection, playerSelection) {
+function singleRound () {
+    const computerSelection = getComputerChoice();
+    let playerSelection = prompt("Choose your move").toLowerCase();
+
+    while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+        playerSelection = prompt("Please enter rock, paper or scissors");}
+
         if (computerSelection === playerSelection) {
         console.log("It's a draw!");
-        } else if (computerSelection === 'rock' && playerSelection === 'paper' || computerSelection === 'paper' && playerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'rock') {
-        console.log("Player wins!");
-        } else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log("Computer wins!");
+        winner = "none";
+
+        } else if (computerSelection === 'rock' && playerSelection === 'paper'
+        || computerSelection === 'paper' && playerSelection === 'scissors'
+        || computerSelection === 'scissors' && playerSelection === 'rock') {
+        console.log("Player wins round!");
+        winner = "player";
+
+        } else if (playerSelection === 'rock' && computerSelection === 'paper'
+        || playerSelection === 'paper' && computerSelection === 'scissors'
+        || playerSelection === 'scissors' && computerSelection === 'rock') {
+        console.log("Computer wins round!");
+        winner = "computer";
     }
 } 
 
-console.log(singleRound(computerSelection, playerSelection));
+
+function game() {
+    let computerScore = 0;
+    let playerScore = 0;
+    for (let i = 0; i < 5; i++) {
+            singleRound();
+            console.log(winner);
+            if (winner === "player") {
+                playerScore++;
+            } else if (winner === "computer") {
+                computerScore++;
+            }
+    } 
+    console.log(computerScore);  
+    console.log(playerScore);
+}
+
+console.log(game());
