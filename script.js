f933a5b0d067df2203e97eed4956f6644b534dce
Claire Roberts <clairefinnie@gmail.com>
2022-07-27 21:26:43 -0400

    added while loop to ensure player selection was valid, and first pass at function for a single round

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-const moveSelections = ['Rock', 'Paper', 'Scissors'];
+const moveSelections = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice () {
     let choice = Math.floor(Math.random() * 3);
-    console.log(moveSelections[choice]);
+    let computerChoice = moveSelections[choice];
+    return computerChoice;
 }
 
-getComputerChoice ();
+const computerSelection = getComputerChoice();
+let playerSelection = prompt("Choose your move").toLowerCase();
+
+while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+    playerSelection = prompt("Please enter rock, paper or scissors");}
+
+function singleRound (computerSelection, playerSelection) {
+        if (computerSelection === playerSelection) {
+        console.log("It's a draw!");
+        } else if (computerSelection === 'rock' && playerSelection === 'paper' || computerSelection === 'paper' && playerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'rock') {
+        console.log("Player wins!");
+        } else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
+        console.log("Computer wins!");
+    }
+} 
+
+console.log(singleRound(computerSelection, playerSelection));
