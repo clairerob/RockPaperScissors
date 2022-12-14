871c9082f448a84acf547f179db9012bbe00893c
Claire Roberts <clairefinnie@gmail.com>
2022-12-14 13:53:34 -0500

    work on ui, final stage and transitions remaining

--- script.js
+++ script.js
@@ -1,58 +1,62 @@
-const MOVE_SELECTIONS = ['rock', 'paper', 'scissors']
-const paper = document.querySelector('.paper')
-const scissors = document.querySelector('.scissors')
-const rock = document.querySelector('.rock')
-const buttons = [paper, scissors, rock]
+const moveButtons = Array.from(document.querySelectorAll('.move-button'))
+moveButtons.forEach((btn) => btn.addEventListener('click', singleRound))
+
+const computer = document.querySelector('#computer-guess')
+const computerText = document.querySelector('#computer-text')
 
 const newRound = document.querySelector('.new-round')
 const resetBtn = document.querySelector('.reset-game')
+newRound.addEventListener('click', updateNewRoundUI)
+resetBtn.addEventListener('click', resetGame)
 
 const backgroundShape = document.querySelector('.background-shape')
 const roundOver = document.querySelector('.round-over')
 
 const scoreDisplay = document.querySelector('.header-total')
 
+const MOVE_SELECTIONS = ['rock', 'paper', 'scissors']
 let currentScore = { computer: 0, player: 0 }
 let winner = ''
 
-paper.addEventListener('click', singleRound)
-scissors.addEventListener('click', singleRound)
-rock.addEventListener('click', singleRound)
-
-newRound.addEventListener('click', newRoundUI)
-resetBtn.addEventListener('click', resetGame)
-
 function getPlayerSelection(e) {
-	console.log('user selection: ' + e.target.dataset.selection)
 	return e.target.dataset.selection
 }
 function getComputerSelection() {
 	let choice = Math.floor(Math.random() * 3)
 	let computerSelection = MOVE_SELECTIONS[choice]
-	console.log('computer selection: ' + computerSelection)
 	return computerSelection
 }
 
 function singleRound(e) {
+	moveButtons.forEach((btn) => btn.removeEventListener('click', singleRound))
+
 	const playerSelection = getPlayerSelection(e)
-	//update UI (step 2)
-	step2UI(playerSelection)
+	updateStep2UI(playerSelection)
 
 	const computerSelection = getComputerSelection()
-	//update UI (step 3)
 	setTimeout(() => {
-		step3UI(computerSelection)
+		updateStep3UI(computerSelection)
 	}, 2000)
 
-	if (computerSelection === playerSelection) {
-		console.log("It's a draw!")
+	findWinnerAndUpdateScore(playerSelection, computerSelection)
+	setTimeout(() => {
+		if (currentScore.computer >= 2 || currentScore.player >= 2) {
+			updateGameOverUI()
+		} else {
+			updateStep4UI()
+		}
+		scoreDisplay.innerText = `${currentScore.player}:${currentScore.computer}`
+	}, 4500)
+}
+
+function findWinnerAndUpdateScore(playerSelection, computerSelection) {
+	if (playerSelection === computerSelection) {
 		winner = 'none'
 	} else if (
 		(computerSelection === 'rock' && playerSelection === 'paper') ||
 		(computerSelection === 'paper' && playerSelection === 'scissors') ||
 		(computerSelection === 'scissors' && playerSelection === 'rock')
 	) {
-		console.log('Player wins round!')
 		winner = 'player'
 		currentScore = { ...currentScore, player: (currentScore.player += 1) }
 	} else if (
@@ -60,81 +64,87 @@ function singleRound(e) {
 		(playerSelection === 'paper' && computerSelection === 'scissors') ||
 		(playerSelection === 'scissors' && computerSelection === 'rock')
 	) {
-		console.log('Computer wins round!')
 		winner = 'computer'
 		currentScore = { ...currentScore, computer: (currentScore.computer += 1) }
 	}
-	console.log(currentScore)
-
-	//update UI (step 4)
-	setTimeout(() => {
-		step4UI(winner)
-		scoreDisplay.innerText = `${currentScore.player}:${currentScore.computer}`
-	}, 4000)
-
-	if (currentScore.computer >= 3) {
-		console.log('COMPUTER WINS')
-		resetGame()
-	} else if (currentScore.player >= 3) {
-		console.log('PLAYER WINS')
-		resetGame()
-	}
 }
 
-function step2UI(playerSelection) {
+function updateStep2UI(playerSelection) {
 	backgroundShape.classList.add('hide')
-	buttons
+	document.getElementById('button-group').style.width = '1000px'
+
+	moveButtons
 		.filter((item) => playerSelection !== item.dataset.selection)
 		.map((item) => item.classList.add('hide'))
-	//add 'player selection' text
-	//add computer placeholder
+
+	const playerBtn = moveButtons.find(
+		(item) => playerSelection === item.dataset.selection
+	)
+	playerBtn.classList.add('current-choice')
+
+	//ADD HIDE TO ALL PT CLASS ELEMENTS
+	const playerText = document.getElementById(`${playerSelection}`)
+	playerText.classList.remove('hide')
+
+	//MAKE PT GLOBAL TO REMOVE ELEMENT
+	//WINNER IMAGE APPENDED TO THIS OR COMPUTER DIV
+	// const playerText = document.createElement('h3')
+	// playerText.innerText = 'YOU PICKED'
+	// playerText.classList.add('player-text')
+	// const playerSelectionDiv = document.getElementById(`${playerSelection}`)
+	// playerSelectionDiv.insertBefore(playerText, playerBtn)
+
+	computer.classList.remove('hide')
+
+	//make btns unclickable
 }
 
-function step3UI(computerSelection) {
-	buttons
-		.find((item) => computerSelection === item.dataset.selection)
-		.classList.remove('hide')
-	//add 'computer selection' text
-	//change display order
-	//handle a draw
+function updateStep3UI(computerSelection) {
+	computer.classList.add('move-button', 'current-choice', computerSelection)
+	computer.classList.remove('blank')
+	computerText.classList.remove('hide')
 }
 
-function step4UI(winner) {
+function updateStep4UI() {
 	let winningText
 	if (winner === 'player') winningText = 'YOU WIN'
 	else if (winner === 'computer') winningText = 'YOU LOSE'
 	else if (winner === 'none') winningText = "IT'S A DRAW"
 	roundOver.classList.remove('hide')
+	newRound.classList.remove('hide')
 	document.querySelector('.winner-text').innerText = winningText
-	//change layout
 	//add winner gradient
+	//spread layout
 }
 
-function newRoundUI() {
-	backgroundShape.classList.remove('hide')
-	buttons.map((item) => item.classList.remove('hide'))
-	roundOver.classList.add('hide')
+function updateGameOverUI() {
+	let winningText
+	if (winner === 'player') winningText = 'GAME OVER - YOU WON'
+	else if (winner === 'computer') winningText = 'GAME OVER - YOU LOSE'
+	roundOver.classList.remove('hide')
+	newRound.classList.add('hide')
+	document.querySelector('.winner-text').innerText = winningText
+	//add more exciting winner UI
 }
+
 function resetGame() {
 	currentScore = { computer: 0, player: 0 }
-	newRoundUI()
+	updateNewRoundUI()
+	scoreDisplay.innerText = `${currentScore.player}:${currentScore.computer}`
 }
 
-// function game() {
-// 	let computerScore = 0
-// 	let playerScore = 0
-// 	for (let i = 0; i < 5; i++) {
-// 		singleRound()
-// 		console.log(winner)
-// 		if (winner === 'player') {
-// 			playerScore++
-// 		} else if (winner === 'computer') {
-// 			computerScore++
-// 		}
-// 	}
-// 	console.log(computerScore)
-// 	console.log(playerScore)
-// }
+function updateNewRoundUI() {
+	backgroundShape.classList.remove('hide')
+	document.getElementById('button-group').style.width = '400px'
+	moveButtons.map((item) => item.classList.remove('hide', 'current-choice'))
+	MOVE_SELECTIONS.map((move) => computer.classList.remove(move))
+	computer.classList.add('blank', 'hide')
+	computerText.classList.add('hide')
+	roundOver.classList.add('hide')
 
+	moveButtons.forEach((btn) => btn.addEventListener('click', singleRound))
+}
+
+function showModal() {}
 //add rules modal
 //use random generator library
